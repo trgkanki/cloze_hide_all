e8be45e3f731f76f9584d8c87235177cb3717bd7
박현우 <phu54321@naver.com>
2024-03-20 02:42:37 +0900

    feat: cloze number extraction on always-visible clozes

--- src/htmlApplier/__init__.py
+++ src/htmlApplier/__init__.py
@@ -9,28 +9,29 @@ from .stripClozeTags import stripClozeTags
 
 def applyClozeTags(html):
     def _(match):
-        clozeNumberString: str = match.group(1)
+        clozeNumber = int(match.group(1))
         clozeContent: str = match.group(2)
         clozeCaption: str = match.group(3) or ""
 
         # Empty cloze - just passthrough
         if not clozeContent:
-            return "{{c%s::%s%s}}" % (clozeNumberString, clozeContent, clozeCaption)
+            return "{{c%d::%s%s}}" % (clozeNumber, clozeContent, clozeCaption)
 
         # Always-shown cloze - just passthrough
         if clozeContent[0] in ("!", "?"):
             clozePrefix = clozeContent[0]
             clozeContent = clozeContent[1:]
-            return "{{c%s::<cz_hide>%s</cz_hide>%s%s}}" % (
-                clozeNumberString,
+            return "{{c%d::<cz_hide class='cz-%d'>%s</cz_hide>%s%s}}" % (
+                clozeNumber,
+                clozeNumber,
                 clozePrefix,
                 clozeContent,
                 clozeCaption,
             )
 
-        return "{{c%s::%s%s}}" % (
-            clozeNumberString,
-            wrapClozeTag(clozeContent, int(clozeNumberString)),
+        return "{{c%d::%s%s}}" % (
+            clozeNumber,
+            wrapClozeTag(clozeContent, int(clozeNumber)),
             clozeCaption,
         )
 
