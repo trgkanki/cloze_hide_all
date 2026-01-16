4d6d623663cb8921cbb8723dfc02c7811004bc09
박현우 <phu54321@naver.com>
2026-01-16 14:05:26 +0900

    fix: using <span> instead for `cz_on_active`
    previously we used <table> for saner integration with Anki's use of `<contenteditable>`,
    but since too many custom note types customize `<table>` (ex: Anking deck) I think that
    we should NOT use `<table>` for formatting purpose. Either use `<div>` or `<span>` is
    the answer. I'm not yet certain, but will try using `<span>` right now.

--- src/assets/editorAddActiveOnlyCSS.js
+++ src/assets/editorAddActiveOnlyCSS.js
@@ -3,14 +3,14 @@ require('anki/RichTextInput').lifecycle.onMount(async ({ customStyles }) => {
   const { element: styleTag } = await addStyleTag('customStyles')
   styleTag.textContent = `
       .cz_on_active {
-        border-collapse: collapse;
-      }
-      .cz_on_active td {
+        display: inline;
         border: 2px dashed #38f;
         border-collapse: collapse;
-        min-height: .5em;
+        min-height: 1em;
         padding: .1em;
       }
-
+      table..cz_on_active, div.cz_on_active {
+        border: 2px dashed #f43;
+      }
     `
 })
--- src/main.py
+++ src/main.py
@@ -211,9 +211,7 @@ def add_buttons(buttons: List[str], editor: Editor) -> None:
     )
 
     def add_conditional_visible_cloze_area(editor: Editor) -> None:
-        editor.web.eval(
-            "wrap('<table class=\"cz_on_active\"><tr><td>', '</td></tr></table>');"
-        )
+        editor.web.eval("wrap('<span class=\"cz_on_active\">', '</span>');")
 
     buttons.append(
         editor.addButton(
