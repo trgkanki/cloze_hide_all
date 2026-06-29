faa254b1c7151025ec0371581f006a0450cbf17f
Park Hyunwoo <phu54321@naver.com>
2026-06-30 01:34:07 +0900

    fix(editorAddActiveOnlyCSS): re-entry trap, unique css name

--- src/assets/editorAddActiveOnlyCSS.js
+++ src/assets/editorAddActiveOnlyCSS.js
@@ -1,15 +1,18 @@
-require('anki/RichTextInput').lifecycle.onMount(async ({ customStyles }) => {
-  const { addStyleTag } = await customStyles
-  const { element: styleTag } = await addStyleTag('customStyles')
-  styleTag.textContent = `
-      .cz_on_active {
-        outline: 2px dashed #38f;
-        outline-offset: -1px;
-        padding: .1em;
-        min-height: 1em;
-      }
-      table.cz_on_active, span.cz_on_active {
-        border: 2px dashed #f43;
-      }
-    `
-})
+if (!window._chaEditorAddActiveOnlyCSSInjected) {
+  window._chaEditorAddActiveOnlyCSSInjected = true
+  require('anki/RichTextInput').lifecycle.onMount(async ({ customStyles }) => {
+    const { addStyleTag } = await customStyles
+    const { element: styleTag } = await addStyleTag('chaStyle')
+    styleTag.textContent = `
+        .cz_on_active {
+          outline: 2px dashed #38f;
+          outline-offset: -1px;
+          padding: .1em;
+          min-height: 1em;
+        }
+        table.cz_on_active, span.cz_on_active {
+          border: 2px dashed #f43;
+        }
+      `
+  })
+}
